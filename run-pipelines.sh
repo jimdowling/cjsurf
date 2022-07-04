c3f52aca2bcd88d68c7aafceda662e57850605c5
Jim Dowling <jim@logicalclocks.com>
2022-07-04 21:00:05 +0200

    updates

--- run-pipelines.sh
+++ run-pipelines.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-jupyter nbconvert --to notebook --execute swell-predictions-feature-pipeline.ipynb
-jupyter nbconvert --to notebook --execute surf-report-feature-pipeline.ipynb
+#jupyter nbconvert --to notebook --execute swell-predictions-feature-pipeline.ipynb
+#jupyter nbconvert --to notebook --execute surf-report-feature-pipeline.ipynb
 jupyter nbconvert --to notebook --execute batch-prediction-pipeline.ipynb
 
