f58c7fc759e10375d44d94552ebb8da03c59f69d
Jim Dowling <jim@logicalclocks.com>
2022-07-07 07:24:44 +0200

    update hopsworks reqs

--- streamlit-image.py
+++ streamlit-image.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import hopsworks
-#from PIL import Image
 
 
 st.title("Lahinch Surf Forecast")
@@ -11,7 +10,4 @@ project = hopsworks.login()
 dataset_api = project.get_dataset_api()
 downloaded_file_path = dataset_api.download("Resources/latest_lahinch.png", overwrite=True)
 
-#image = Image.open(downloaded_file_path)
-
-#st.image(image, caption='Lahinch Surf Forecast')
 st.image(downloaded_file_path, caption='Lahinch Surf Forecast')
