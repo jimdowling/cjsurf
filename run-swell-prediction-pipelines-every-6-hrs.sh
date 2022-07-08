eb1c3731b76e90b7ab8cedba78e9d6df90467969
Jim Dowling <jim@logicalclocks.com>
2022-07-08 08:58:00 +0200

    make it 2 github actions

--- run-swell-prediction-pipelines-every-6-hrs.sh
+++ run-swell-prediction-pipelines-every-6-hrs.sh
@@ -4,8 +4,6 @@ set -e
 
 echo "Running swell predictions feature pipeline"
 jupyter nbconvert --to notebook --execute swell-predictions-feature-pipeline.ipynb
-echo "Running surf report feature pipeline"
-jupyter nbconvert --to notebook --execute surf-report-feature-pipeline.ipynb
 echo "Running batch prediction pipeline"
 jupyter nbconvert --to notebook --execute batch-prediction-pipeline.ipynb
 
