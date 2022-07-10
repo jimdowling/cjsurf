e12a23ad93c6ecf4f62364991d9a7199fbea190d
Jim Dowling <jim@logicalclocks.com>
2022-07-10 21:04:56 +0200

    fix

--- scripts/run-surf-report-pipeline-24-hrs.sh
+++ scripts/run-surf-report-pipeline-24-hrs.sh
@@ -5,5 +5,5 @@ set -e
 echo "Running surf report feature pipeline"
 cd notebooks
 
-jupyter nbconvert --to notebook --execute notebooks/surf-report-feature-pipeline.ipynb
+jupyter nbconvert --to notebook --execute surf-report-feature-pipeline.ipynb
 
