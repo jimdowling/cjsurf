030da83d6e3abd2ce55762885de7328323b58c09
Jim Dowling <jim@logicalclocks.com>
2022-08-21 10:47:22 +0200

    rewrite for batch

--- scripts/run-surf-report-pipeline-24-hrs.sh
+++ scripts/run-surf-report-pipeline-24-hrs.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Running surf report feature pipeline"
 cd notebooks
-
+export CJSURF_BACKFILL="False"
 jupyter nbconvert --to notebook --execute surf-report-feature-pipeline.ipynb
 
--- scripts/run-swell-prediction-pipelines-every-6-hrs.sh
+++ scripts/run-swell-prediction-pipelines-every-6-hrs.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd notebooks
-
+export CJSURF_BACKFILL="False"
 echo "Running swell predictions feature pipeline"
 jupyter nbconvert --to notebook --execute swell-predictions-feature-pipeline.ipynb
 echo "Running batch prediction pipeline"
