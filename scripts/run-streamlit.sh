ceeb6262dfcb33b4b06328933152d91bce0896ee
Jim Dowling <jim@logicalclocks.com>
2022-08-29 22:42:32 +0200

    cleanup

--- scripts/run-streamlit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export HOPSWORKS_API_KEY=$(cat api-key.txt)
-
-python -m streamlit run streamlit-image.py
--- scripts/run-surf-report-pipeline-24-hrs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Running surf report feature pipeline"
-cd notebooks
-
-jupyter nbconvert --to notebook --execute surf-report-feature-pipeline.ipynb
-
